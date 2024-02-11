ccd23bfc20475111d1401b22f64acf563a8e0116
wecraw <william.crawford6@t-mobile.com>
2024-02-10 16:11:35 -0800

    working download button

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss'],
 })
-export class ResumeComponent {}
+export class ResumeComponent {
+  download() {
+    const url =
+      'https://raw.githubusercontent.com/wecraw/resume/356094c18bb079a0e4398081dcf66916357c4153/wec_resume_2024.pdf';
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', '');
+    link.click();
+  }
+}
